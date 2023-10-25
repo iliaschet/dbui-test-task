a96d4d6c1b09a2a632a2c1bbc24d79ccb457c0fb
Ilya Schetchikov <schetchikov.i.e@sbertech.ru>
2023-10-25 10:54:37 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dbui-test-task
+
+/backend -> npm run start 
+/frontend -> npm run start
---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export const Dashboard = () => {
+  return (
+    <Typography variant="h2" component="h2" textAlign={"center"}>
+      Coming soon
+    </Typography>
+  );
+};
