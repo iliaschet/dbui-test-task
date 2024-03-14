cb9e04dacf42fce9155e76bd167b17a80d2dfd35
stasikrus <stas-boss@mail.ru>
2024-03-14 19:30:36 +0300

    fix: corrected API endpoint address

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Data } from "./types";
 
-const BASE_URL = "http://localhost:5000/api/v11";
+const BASE_URL = "http://localhost:5000/api/v1";
 
 export const userApi = createApi({
   reducerPath: "userApi",
